dff7ee2ed7e7862bd89127ddfafce8f5ebdd1fef
KRKE <136760702+G080312@users.noreply.github.com>
2023-06-27 11:30:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-numbers = [100,4,1,9,12,52,58,88,10,25,300,29,63,71,22,11,35,39,8,41,45,6,9]
-data = sorted(numbers)
-half = len(data) // 2
-box,outlier = [],[]
-num,count,total,quartile1 = 0,0,0,0
-
-for i in range(0,len(data)):
-  total += data[i]
-value = round(total / len(data),1)
-
-for number in range(0,len(data)):
-  if (value * 3) <= data[number]:
-    outlier.append(data[number])
-
-if len(data) % 2 == 0:
-  median = (data[half - 1] + data[half]) / 2
-else:
-  median = data[half]
-
-if (half / 2).is_integer():
-  for i in range(half):
-    if i == (half // 2) - 1:
-      num += data[i]
-    elif i == half // 2:
-      num += data[i]
-  quartile1 = num / 2
-  for j in range(len(data)):
-    if j >= half + 1:
-      box.append(data[j])
-      count += 1
-  quartile3 = (box[count // 2] + box[(count // 2) - 1]) / 2
-else:
-  for i in range(half):
-    if i == (half // 2):
-      quartile1 += data[i]
-  for j in range(len(data)):
-    if j >= half:
-      box.append(data[j])
-      count += 1
-  quartile3 = box[count // 2]
+import time
+def Main(Numerical):
+  global Numbers,data,Total,mode
+  global Range25per,Range50per,Range75per
+  global Outlier,Value,Scope,Quartilerange
+  global Average,Max,Min,Len,Record,half,Type
+  global TaskTime
+  start = time.time()
+  if type(Numerical) == list:Numbers,Type = Numerical,list
+  elif type(Numerical) == str:Numbers,Type = eval(Numerical),str
+  else:Numbers,Type = Numerical,None
+  data = sorted(Numbers)
+  half = len(data) // 2
+  box,Outlier = [],[]
+  Sum,Record,Total,Range25per,max = 0,0,0,0,0
+  for i in range(len(data)):Total += data[i]
+  Value = Total / len(data)
+  for number in range(len(data)):
+    if Value * 3 <= data[number]:Outlier.append(data[number])
+  if len(data) % 2 == 0:Range50per = (data[half - 1] + data[half]) / 2
+  else:Range50per = data[half]
+  if (half / 2).is_integer():
+    for i in range(half):
+      if i == (half // 2) - 1:Sum += data[i]
+      elif i == half // 2:Sum += data[i]
+    Range25per = Sum / 2
+    for j in range(len(data)):
+      if j >= half + 1:
+        box.append(data[j])
+        Record += 1
+    Range75per = (box[Record // 2] + box[(Record // 2) - 1]) / 2
+  else:
+    for i in range(half):
+      if i == half // 2:Range25per += data[i]
+    for j in range(len(data)):
+      if j >= half:
+        box.append(data[j])
+        Record += 1
+    Range75per = box[Record // 2]
+  Scope = data[len(data)-1] - data[0]
+  Quartilerange = Range75per - Range25per
+  Min,Max = data[0],data[len(data)-1]
+  Average,Len = round(Value,1),len(data)
+  for d in data:
+    data_count = data.count(d)
+    if data_count > max:max,mode = data_count,d
+  end = time.time()
+  TaskTime = end - start
