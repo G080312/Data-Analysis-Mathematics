cd2bef14965d9db6453c7b4ab1fe88232bbdb453
KRKE <136760702+G080312@users.noreply.github.com>
2023-06-27 19:40:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 def Main(Numerical):
   global Numbers,data,Total,mode
   global Range25per,Range50per,Range75per
-  global Outlier,Value,Scope,Quartilerange
+  global Outlier,Scope,Quartilerange
   global Average,Max,Min,Len,Record,half,Type
   global TaskTime
   start = time.time()
@@ -14,9 +14,9 @@ def Main(Numerical):
   box,Outlier = [],[]
   Sum,Record,Total,Range25per,max = 0,0,0,0,0
   for i in range(len(data)):Total += data[i]
-  Value = Total / len(data)
+  Average = Total / len(data)
   for number in range(len(data)):
-    if Value * 3 <= data[number]:Outlier.append(data[number])
+    if Average * 3 <= data[number]:Outlier.append(data[number])
   if len(data) % 2 == 0:Range50per = (data[half - 1] + data[half]) / 2
   else:Range50per = data[half]
   if (half / 2).is_integer():
@@ -40,7 +40,7 @@ def Main(Numerical):
   Scope = data[len(data)-1] - data[0]
   Quartilerange = Range75per - Range25per
   Min,Max = data[0],data[len(data)-1]
-  Average,Len = round(Value,1),len(data)
+  Len = len(data)
   for d in data:
     data_count = data.count(d)
     if data_count > max:max,mode = data_count,d
