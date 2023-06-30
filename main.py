2b65d091e82020d40d0260915b697b6c7b97d3fd
KRKE <136760702+G080312@users.noreply.github.com>
2023-06-30 15:43:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,22 @@
-import time
-def Main(Numerical):
-  global Numbers,data,Total,mode
-  global Range25per,Range50per,Range75per
-  global Outlier,Scope,Quartilerange
-  global Average,Max,Min,Len,Record,half,Type
-  global TaskTime
-  start = time.time()
-  if type(Numerical) == list:Numbers,Type = Numerical,list
-  elif type(Numerical) == str:Numbers,Type = eval(Numerical),str
-  else:Numbers,Type = Numerical,None
-  data = sorted(Numbers)
-  half = len(data) // 2
-  box,Outlier = [],[]
-  Sum,Record,Total,Range25per,max = 0,0,0,0,0
-  for i in range(len(data)):Total += data[i]
-  Average = Total / len(data)
-  for number in range(len(data)):
-    if Average * 3 <= data[number]:Outlier.append(data[number])
-  if len(data) % 2 == 0:Range50per = (data[half - 1] + data[half]) / 2
-  else:Range50per = data[half]
-  if (half / 2).is_integer():
-    for i in range(half):
-      if i == (half // 2) - 1:Sum += data[i]
-      elif i == half // 2:Sum += data[i]
-    Range25per = Sum / 2
-    for j in range(len(data)):
-      if j >= half + 1:
-        box.append(data[j])
-        Record += 1
-    Range75per = (box[Record // 2] + box[(Record // 2) - 1]) / 2
-  else:
-    for i in range(half):
-      if i == half // 2:Range25per += data[i]
-    for j in range(len(data)):
-      if j >= half:
-        box.append(data[j])
-        Record += 1
-    Range75per = box[Record // 2]
-  Scope = data[len(data)-1] - data[0]
-  Quartilerange = Range75per - Range25per
-  Min,Max = data[0],data[len(data)-1]
-  Len = len(data)
-  for d in data:
-    data_count = data.count(d)
-    if data_count > max:max,mode = data_count,d
-  end = time.time()
-  TaskTime = end - start
+from npy import calc,calc2data
+data1 = [1, 2, 3, 4, 5] # or data1 = "1,2,3,4,5"
+data2 = [5,6,8,5,7] # or data2 = "5,6,8,5,7"
+
+data1resp = calc2data(data1)
+data2resp = calc2data(data2)
+print(f"\n分散(s²) = {data2resp['variance']}\n標準偏差 = √{data2resp['standard_deviation']}")
+print(f"Data1: 平均 = {data1resp['average']:.2f}, 分散 = {data1resp['variance']:.2f}, 標準偏差 = {data1resp['standard_deviation']:.2f}")
+print(f"Data2: 平均 = {data2resp['average']:.2f}, 分散 = {data2resp['variance']:.2f}, 標準偏差 = {data2resp['standard_deviation']:.2f}")
+result = calc(data1)
+print('平均値:', result['average'])
+print('最大値:', result['max'])
+print('最小値:', result['min'])
+print('中央値:', result['median'])
+print('第1四分位数:', result['Q1'])
+print('第2四分位数:', result['Q2'])
+print('第3四分位数:', result['Q3'])
+print('四分位範囲:', result['IQR'])
+print('範囲:', result['range'])
+print('最頻値:', result['mode'])
+print('外れ値:', result['outliers'])
+print('処理時間:', result['task_time'])
