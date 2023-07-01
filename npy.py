48aa40eae7c064c1195946c2f038d2f880a47203
KRKE <136760702+G080312@users.noreply.github.com>
2023-07-01 14:45:30 +0900

    Update npy.py

--- npy.py
+++ npy.py
@@ -3,44 +3,45 @@ import math
 
 def calc(Numerical):
   start = time.time()
-  if type(Numerical) == list:Numbers = Numerical
-  elif type(Numerical) == str:Numbers = eval(Numerical)
-  else:Numbers = Numerical
-  data = sorted(Numbers)
-  half = len(data) // 2
+  if type(Numerical) == list:data = Numerical
+  elif type(Numerical) == str:data = eval(Numerical)
+  else:data = Numerical
+  sorted_data = sorted(data)
+  half = len(sorted_data) // 2
   box, Outlier = [], []
   Sum, Record, Total, Range25per, max_count = 0, 0, 0, 0, 0
-  for i in range(len(data)):Total += data[i]
-  Average = Total / len(data)
-  for number in range(len(data)):
-    if Average * 3 <= data[number]:Outlier.append(data[number])
-  if len(data) % 2 == 0:Range50per = (data[half - 1] + data[half]) / 2
-  else:Range50per = data[half]
+  for i in range(len(sorted_data)):Total += sorted_data[i]
+  Average = Total / len(sorted_data)
+  for number in range(len(sorted_data)):
+    if Average * 3 <= sorted_data[number]:
+      Outlier.append(sorted_data[number])
+  if len(sorted_data) % 2 == 0:Range50per = (sorted_data[half - 1] + sorted_data[half]) / 2
+  else:Range50per = sorted_data[half]
   if (half / 2).is_integer():
     for i in range(half):
-      if i == (half // 2) - 1:Sum += data[i]
-      elif i == half // 2:Sum += data[i]
+      if i == (half // 2) - 1:Sum += sorted_data[i]
+      elif i == half // 2:Sum += sorted_data[i]
     Range25per = Sum / 2
-    for j in range(len(data)):
+    for j in range(len(sorted_data)):
       if j >= half + 1:
-        box.append(data[j])
+        box.append(sorted_data[j])
         Record += 1
     Range75per = (box[Record // 2] + box[(Record // 2) - 1]) / 2
   else:
     for i in range(half):
-      if i == half // 2:Range25per += data[i]
-    for j in range(len(data)):
+      if i == half // 2:Range25per += sorted_data[i]
+    for j in range(len(sorted_data)):
       if j >= half:
-        box.append(data[j])
+        box.append(sorted_data[j])
         Record += 1
     Range75per = box[Record // 2]
-  Scope = data[len(data)-1] - data[0]
+  Scope = sorted_data[len(sorted_data)-1] - sorted_data[0]
   Quartilerange = Range75per - Range25per
-  Min_value, Max_value = data[0], data[len(data)-1]
-  Len = len(data)
+  Min_value, Max_value = sorted_data[0], sorted_data[len(sorted_data)-1]
+  Len = len(sorted_data)
   mode_value = None
-  for d in data:
-    data_count = data.count(d)
+  for d in sorted_data:
+    data_count = sorted_data.count(d)
     if data_count > max_count:max_count, mode_value = data_count, d
   end = time.time()
   TaskTime = end - start
@@ -50,23 +51,23 @@ def calc(Numerical):
     'max': Max_value,
     'min': Min_value,
     'median': Range50per,
-    'Q1': Range25per,
-    'Q2': Range50per,
-    'Q3': Range75per,
     'IQR': Quartilerange,
     'range': Scope,
     'mode': mode_value,
     'outliers': Outlier,
+    'Q1': Range25per,
+    'Q2': Range50per,
+    'Q3': Range75per,
     'task_time': TaskTime
   }
   return result
 
 def calc2data(data):
   start = time.time()
-  if type(data) == list:num_data, data_type = data, list
-  elif type(data) == str:num_data, data_type = eval(data), str
+  if type(data) == list:num_data, data_type = data,list
+  elif type(data) == str:num_data, data_type = eval(data),str
   else:num_data, data_type = data, None
-  sorted_data, total,deviation_total = sorted(num_data), 0 , 0
+  sorted_data, total,deviation_total = sorted(num_data),0,0
   for value in sorted_data:total += value
   average = total / len(sorted_data)
   deviation_list,output_deviation,deviation_squared_list = [],[],[]
@@ -78,7 +79,7 @@ def calc2data(data):
     deviation_squared_list.append(deviation_squared)
     deviation_total += deviation_squared
   variance = deviation_total / len(sorted_data)
-  deviation_str = '+'.join(output_deviation)
+  deviation_str = '+'.join(output_deviation).replace(".0","")
   data_len = len(sorted_data)
   standard_deviation = math.sqrt(variance)
   end = time.time()
@@ -91,5 +92,32 @@ def calc2data(data):
     'deviation_str': deviation_str,
     'data_type': data_type,
     'TaskTime': TaskTime
+  }
+  return result
+
+def merge(x,y):
+  deviation1st,Quartile1st = calc2data(x),calc(x)
+  deviation2nd,Quartile2nd = calc2data(y),calc(x)
+  result = {
+    'deviation[1]':deviation1st,
+    'Quartile[1]':Quartile1st,
+    'deviation[2]':deviation2nd,
+    'Quartile[2]':Quartile2nd
+  }
+  return result
+
+def prime(start,end):
+  global primes
+  number = range(start,end)
+  def prime(num):
+    for x in range(2,num):
+      if (num % x) == 0:
+        return False
+    return True
+  primes = list(filter(prime,number))
+  result = {
+    'primes': primes,
+    'start_num': start,
+    'end_num': end
   }
   return result
