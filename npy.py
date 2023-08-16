439d12daaf1bd34a4c1c14581a0ef328ec3000da
KRKE <136760702+G080312@users.noreply.github.com>
2023-08-16 22:49:27 +0900

    Update npy.py

--- npy.py
+++ npy.py
@@ -1,48 +1,49 @@
 import math
 
-def calc(data):
+def statistics(data):
   if type(data) == str:
     data = data.strip('[]').split(',')
     data = [float(x) for x in data]
   sorted_data = sorted(data)
-  half = len(sorted_data) // 2
+  length = len(sorted_data)
+  half = length // 2
   box, Outlier = [], []
-  Sum, Record, Total, Range25per, max_count, mode_value = 0,0,0,0,0,None
-  for i in range(len(sorted_data)):Total += sorted_data[i]
-  Average = Total / len(sorted_data)
-  for number in range(len(sorted_data)):
+  count, box_count, Total, Range25per, max_count = 0,0,0,0,0
+  mode_value = None
+  Total = sum(sorted_data)
+  Average = Total / length
+  for number in range(length):
     if Average * 3 <= sorted_data[number]:
       Outlier.append(sorted_data[number])
-  if len(sorted_data) % 2 == 0: Range50per = (sorted_data[half - 1] + sorted_data[half]) / 2
+  if length % 2 == 0: Range50per = (sorted_data[half - 1] + sorted_data[half]) / 2
   else: Range50per = sorted_data[half]
   if (half / 2).is_integer():
-    for i in range(half):
-      if i == (half // 2) - 1:Sum += sorted_data[i]
-      elif i == half // 2:Sum += sorted_data[i]
-    Range25per = Sum / 2
-    for j in range(len(sorted_data)):
+    for h in range(half):
+      if h == (half // 2) - 1:count += sorted_data[h]
+      elif h == half // 2:count += sorted_data[h]
+    Range25per = count / 2
+    for j in range(length):
       if j >= half + 1:
         box.append(sorted_data[j])
-        Record += 1
-    Range75per = (box[Record // 2] + box[(Record // 2) - 1]) / 2
+        box_count += 1
+    Range75per = (box[box_count // 2] + box[(box_count // 2) - 1]) / 2
   else:
-    for i in range(half):
-      if i == half // 2:Range25per += sorted_data[i]
-    for j in range(len(sorted_data)):
+    for h in range(half):
+      if h == half // 2:Range25per += sorted_data[h]
+    for j in range(length):
       if j >= half:
         box.append(sorted_data[j])
-        Record += 1
-    Range75per = box[Record // 2]
-  Scope = sorted_data[len(sorted_data) - 1] - sorted_data[0]
+        box_count += 1
+    Range75per = box[box_count // 2]
+  Scope = sorted_data[length - 1] - sorted_data[0]
   Quartilerange = Range75per - Range25per
-  Min_value, Max_value = sorted_data[0], sorted_data[len(sorted_data) - 1]
-  Len = len(sorted_data)
+  Min_value, Max_value = sorted_data[0], sorted_data[length - 1]
   for d in sorted_data:
     data_count = sorted_data.count(d)
     if data_count > max_count:max_count,mode_value = data_count,d
   result = {
-    'len': Len,
-    'Total': Total,
+    'len': length,
+    'total': Total,
     'average': Average,
     'max': Max_value,
     'min': Min_value,
@@ -66,43 +67,39 @@ def Difference(data):
   if type(data) == str:
     data = data.strip('[]').split(',')
     data = [float(x) for x in data]
-  deviation_list,deviation_squared_list = [],[]
-  sorted_data, total,deviation_total = sorted(data),0,0
-  data_len = len(sorted_data)
-  for value in sorted_data:total += value
-  average = total / data_len
-  for value in sorted_data:
-    deviation = value - average
-    deviation_list.append(deviation)
-    deviation_squared = deviation ** 2
-    deviation_squared_list.append(deviation_squared)
-    deviation_total += deviation_squared
-  variance = deviation_total / data_len
+  sorted_data = sorted(data)
+  length = len(sorted_data)
+  total = sum(sorted_data)
+  average = total / length
+  deviation_list = [value - average for value in sorted_data]
+  deviation_squared_list = [deviation ** 2 for deviation in deviation_list]
+  deviation_total = sum(deviation_squared_list)
+  variance = deviation_total / length
   standard_deviation = math.sqrt(variance)
   result = {
     'sorted_data': sorted_data,
-    'total':total,
-    'len': data_len,
+    'total': total,
+    'len': length,
     'average': average,
     'variance': variance,
     'deviation_list': deviation_list,
     'standard_deviation': standard_deviation,
-    'deviation_squared_list': deviation_squared_list,
+    'deviation_squared_list': deviation_squared_list
   }
   return result
 
 def merge(x,y):
-  difference1st,Quartile1st = Difference(x),calc(x)
-  difference2nd,Quartile2nd = Difference(y),calc(y)
+  difference1,Quartile1 = Difference(x),statistics(x)
+  difference2,Quartile2 = Difference(y),statistics(y)
   result = {
     'data[1]': {
-      'Quartile': Quartile1st,
-      'deviation': difference1st,
+      'Quartile': Quartile1,
+      'deviation': difference1,
     },
     'data[2]': {
-      'Quartile': Quartile2nd,
-      'deviation': difference2nd,
-    },
+      'Quartile': Quartile2,
+      'deviation': difference2,
+    }
   }
   return result
 
